e90b2f81030d07d934e2dcfef1bfb32eb8310070
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-08-05 23:11:10 -0400

    Updating the notebook and schema query

--- wine_db_schema.sql
+++ wine_db_schema.sql
@@ -1,72 +1,80 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+﻿-- Database: wine_review_db
 
+-- DROP DATABASE wine_review_db;
 
-CREATE TABLE "country" (
-    "country_id" int   NOT NULL,
-    "country_name" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_country" PRIMARY KEY (
-        "country_id"
+CREATE DATABASE wine_review_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+-- CREATE TABLES
+
+CREATE TABLE IF NOT EXISTS "winery" (
+    "winery_id" int   NOT NULL,
+    "winery_name" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_winery" PRIMARY KEY (
+        "winery_id"
      )
 );
 
-CREATE TABLE "grape_variety" (
+CREATE TABLE IF NOT EXISTS "taster" (
+    "taster_id" int   NULL,
+    "taster_name" varchar(50)   NULL,
+    "taster_twitter" varchar(50)   NULL,
+    CONSTRAINT "pk_taster" PRIMARY KEY (
+        "taster_id"
+     )
+);
+
+CREATE TABLE IF NOT EXISTS "wine_review" (
+    "wine_review_id" int   NOT NULL,
+    "wine_name" varchar(200)   NULL,
+    "wine_year" int   NULL,
+    "wine_price" decimal(10,2)   NULL,
+    "wine_score" int   NULL,
     "grape_id" int   NOT NULL,
-    "grape_name" varchar(30)   NOT NULL,
-    "grape_color" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_grape_variety" PRIMARY KEY (
-        "grape_id"
+    "region_id" int   NOT NULL,
+    "taster_id" int   NOT NULL,
+    "winery_id" int   NOT NULL,
+    CONSTRAINT "pk_wine_review" PRIMARY KEY (
+        "wine_review_id"
      )
 );
 
-CREATE TABLE "region" (
+CREATE TABLE IF NOT EXISTS "region" (
     "region_id" int   NOT NULL,
-    "province" varchar(30)   NOT NULL,
-    "region1" varchar(30)   NOT NULL,
-    "region2" varchar(30)   NOT NULL,
-    "combined_region" varchar(100)   NOT NULL,
-    "latitude" decimal(8,6)   NOT NULL,
-    "longitude" decimal(9,6)   NOT NULL,
+    "province" varchar(50)   NULL,
+    "region1" varchar(50)   NULL,
+    "region2" varchar(50)   NULL,
+    "combined_region" varchar(200)   NOT NULL,
+    "latitude" decimal(8,6)   NULL,
+    "longitude" decimal(9,6)   NULL,
     "country_id" int   NOT NULL,
     CONSTRAINT "pk_region" PRIMARY KEY (
         "region_id"
      )
 );
 
-CREATE TABLE "wine_review" (
-    "wine_id" int   NOT NULL,
-    "wine_name" varchar(50)   NOT NULL,
-    "wine_year" int   NOT NULL,
-    "wine_price" decimal(10,2)   NOT NULL,
-    "wine_score" int   NOT NULL,
+CREATE TABLE IF NOT EXISTS "country" (
+    "country_id" int   NOT NULL,
+    "country_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_country" PRIMARY KEY (
+        "country_id"
+     )
+);
+
+CREATE TABLE IF NOT EXISTS "grape_variety" (
     "grape_id" int   NOT NULL,
-    "region_id" int   NOT NULL,
-    "taster_id" int   NOT NULL,
-    "winery_id" int   NOT NULL,
-    CONSTRAINT "pk_wine_review" PRIMARY KEY (
-        "wine_id"
+    "grape_name" varchar(50)   NULL,
+    "grape_color" varchar(15)   NULL,
+    CONSTRAINT "pk_grape_variety" PRIMARY KEY (
+        "grape_id"
      )
 );
-
-CREATE TABLE "taster" (
-    "taster_id" int   NOT NULL,
-    "taster_name" varchar(30)   NOT NULL,
-    "taster_twitter" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_taster" PRIMARY KEY (
-        "taster_id"
-     )
-);
-
-CREATE TABLE "winery" (
-    "winery_id" int   NOT NULL,
-    "winery_name" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_winery" PRIMARY KEY (
-        "winery_id"
-     )
-);
-
-ALTER TABLE "region" ADD CONSTRAINT "fk_region_country_id" FOREIGN KEY("country_id")
-REFERENCES "country" ("country_id");
 
 ALTER TABLE "wine_review" ADD CONSTRAINT "fk_wine_review_grape_id" FOREIGN KEY("grape_id")
 REFERENCES "grape_variety" ("grape_id");
@@ -79,4 +87,11 @@ REFERENCES "taster" ("taster_id");
 
 ALTER TABLE "wine_review" ADD CONSTRAINT "fk_wine_review_winery_id" FOREIGN KEY("winery_id")
 REFERENCES "winery" ("winery_id");
+
+ALTER TABLE "region" ADD CONSTRAINT "fk_region_country_id" FOREIGN KEY("country_id")
+REFERENCES "country" ("country_id");
+
+
+
+
 
