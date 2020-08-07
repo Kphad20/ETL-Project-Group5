c632893417c899d17cfd2b014a546be61e11dd12
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-08-06 22:10:45 -0400

    Created notebook for creating province.csv containing lat and long info.. Imported province.csv and updated schema.sql

--- wine_db_schema.sql
+++ wine_db_schema.sql
@@ -1,17 +1,4 @@
-﻿-- Database: wine_review_db
-
--- DROP DATABASE wine_review_db;
-
-CREATE DATABASE wine_review_db
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-	
--- CREATE TABLES
+﻿-- CREATE TABLES
 
 CREATE TABLE IF NOT EXISTS "winery" (
     "winery_id" int   NOT NULL,
